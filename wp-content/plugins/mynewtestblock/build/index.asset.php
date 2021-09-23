fe77f242dde44f9ad8bd18ae0a277f670e7f9b7d
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-09-23 14:50:24 +0200

    Initial commit

---
+++ wp-content/plugins/mynewtestblock/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '692c2ebec4046fdfdd4f0c0b2c049ab5');
---
+++ wp-content/plugins/mynewtestblock/src/save.js
@@ -0,0 +1,8 @@
+export default function save(props) {
+	console.log({props})
+	return (
+		<p className="wp-block-create-block-mynewtestblock">
+				'My New Test Block – hello from the saved content! update lol'
+		</p>
+	);
+}
