b8c3641895c33b5d1fc194ec216e4db507a4950d
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-09-26 20:52:31 +0200

    Update tailwind workflow

--- wp-content/themes/tailpress/functions.php
+++ wp-content/themes/tailpress/functions.php
@@ -1,8 +1,7 @@
 <?php
+
 declare(strict_types=1);
 
-add_action('wp_enqueue_scripts', function() {
-    wp_enqueue_script('main', get_stylesheet_directory_uri() . '/assets/dist/main.bundle.js', [], '1.0.0', true);
-    // wp_enqueue_style('style', get_stylesheet_directory_uri() . '/assets/dist/main.css', [], '1.0.0', true);
-    wp_enqueue_style('style', get_theme_file_uri('assets/dist/main.css'));
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('style', get_theme_file_uri('dist.css'));
 });
